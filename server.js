2f0fd34767a6fe0eb999aa96e6f45cf42689c23b
Kristian <56667566+kristian821@users.noreply.github.com>
2021-04-08 22:41:27 -0700

    Added Heroku

--- server.js
+++ server.js
@@ -40,7 +40,12 @@ const filterByQuery = (query, animalsArray) => {
     }
     // Return the filtered results
     return filteredResults;
-}
+};
+
+const findById = (id, animalsArray) => {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -49,6 +54,16 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+    
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
